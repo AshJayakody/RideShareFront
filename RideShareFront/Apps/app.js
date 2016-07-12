e04d2cfe508c952871205f0e51238d40d99f305a
AshJayakody <ashajayakody@gmail.com>
2016-07-12 23:55:31 +0530

    login done

--- RideShareFront/Apps/app.js
+++ RideShareFront/Apps/app.js
@@ -75,38 +75,38 @@ app.controller('loginController', [ '$scope' , '$http' , function ($scope,$http)
                 });
 
         }
-    };
+    }
 
   
 }]);
 
 
-app.controller('registerController',[ '$scope' , '$http' , function ($scope,$http) {
+app.controller('registerController', ['$scope', '$http', function ($scope, $http) {
     $scope.registererrorMessages = "";
 
-        $scope.submit = function () {
-            if ($scope.registerFirstName &&  $scope.registerLastName && $scope.registerUserName && $scope.registerEmail && $scope.registerPassword) {
-                var model = {
-                    "FirstName": $scope.registerFirstName,
-                    "LastName": $scope.registerLastName,
-                    "UserName": $scope.registerUserName,
-                    "Email": $scope.registerEmail,
-                    "Password": $scope.registerPassword
-                }
-                $http.post('http://localhost:63603//api/register/post', model).
-                    success(function (data, status, headers, config) {
-                        alert('Registered Successfully!');
-                        //window.location = "/View/Login.html";
-                    }).
-                    error(function (data, status, headers, config) {
-                        $scope.registererrorMessages = data;
-                        //alert("error");
-                       
-                    });
-
+    $scope.submit = function () {
+        if ($scope.registerFirstName && $scope.registerLastName && $scope.registerUserName && $scope.registerEmail && $scope.registerPassword) {
+            var model = {
+                "FirstName": $scope.registerFirstName,
+                "LastName": $scope.registerLastName,
+                "UserName": $scope.registerUserName,
+                "Email": $scope.registerEmail,
+                "Password": $scope.registerPassword
             }
-        };
-}])
+            $http.post('http://localhost:63603//api/register/post', model).
+                success(function (data, status, headers, config) {
+                    alert('Registered Successfully!');
+                    //window.location = "/View/Login.html";
+                }).
+                error(function (data, status, headers, config) {
+                    $scope.registererrorMessages = data;
+                    //alert("error");
+
+                });
+
+        }
+    };
+}]);
 
 
     app.controller("showmap", function ($scope, $interval) {
